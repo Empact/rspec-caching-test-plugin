f0f182d817051f032fe21ceaffcc1dbad1edb389
Ben Woosley <ben.woosley@gmail.com>
2010-08-08 23:06:23 -0400

    Revert "only enable caching when we are actually testing it (i.e. within the matchers)"
    This reverts commit 99cba9394c9c55efba19579811e36f16fa611378.

--- lib/matchers.rb
+++ lib/matchers.rb
@@ -17,7 +17,7 @@ module AGW
         end
         
         def matches?(block)
-          temporarily_enable_caching { block.call }
+          block.call
           @controller = @example_group.controller
           @key = cache_key_for_name(@name)
           query_cache_store(@key)
@@ -32,14 +32,6 @@ module AGW
         end
         
       private
-      
-        def temporarily_enable_caching
-          old_setting = ActionController::Base.perform_caching
-          ActionController::Base.perform_caching = true
-          yield
-          ActionController::Base.perform_caching = old_setting
-        end
-        
         # override this method in the concrete subclasses
         def query_cache_store(key)
           raise NotImplementedError
@@ -156,7 +148,7 @@ module AGW
       end
       alias_method :expire_fragment, :expire
       
-      class CachePage < AbstractCacheMatcher #:nodoc:
+      class CachePage #:nodoc:
         def initialize(url)
           @url = url
           ActionController::Base.reset_page_cache!
@@ -164,7 +156,7 @@ module AGW
         
         # See if +ActionController::Base+ was told to cache our page.
         def matches?(block)
-          temporarily_enable_caching { block.call }
+          block.call
           return ActionController::Base.cached?(@url)
         end
 
@@ -198,7 +190,7 @@ module AGW
         end
 
         def matches?(block)
-          temporarily_enable_caching { block.call }
+          block.call 
           return ActionController::Base.cache_store.expired?(@url)
         end
 
